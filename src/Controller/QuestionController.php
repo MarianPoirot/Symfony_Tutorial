1ac34e6459dc522ea9bd36f9f778b405277ef1d7
mapoirot1 <marian.poirot@etu.uca.fr>
2023-04-06 10:03:02 +0200

    Before starting webpack encore

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(){
-        return $this ->render('question/homepage.html.twig');
+    public function homepage(Environment $twigEnvironment){
+
+        //Using twig service directly
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+        return new Response($html);
+        //Using twig Recipe
+        #return $this ->render('question/homepage.html.twig');
+        //Using hardcoded HTML
         #return new Response('Hello there !');
     }
 
